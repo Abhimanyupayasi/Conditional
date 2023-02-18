071c52ff77febb93a262272bb24c17586dff4931
Abhimanyu Payasi <117967027+Abhimanyupayasi@users.noreply.github.com>
2023-02-18 11:27:29 +0530

    Create squareproblem.java
    pw conditional assignment

---
+++ squareproblem.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Square {
+
+public static void main(String[] args) {
+
+
+    System.out.println("Enter length");
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.println("Enter Breadth ");
+    int b = sc.nextInt();
+    if( a == b) {
+        System.out.println( " this is square");
+
+
+    }
+    else {
+        System.out.println("this is not square");
+    }
+    
+        
+
+        
+    }
+    
+}
